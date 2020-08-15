6cb72be2cd05e6c07fd6b0bc55e2f6e3103a2638
Doctor Dalek <69600500+DoctorDalek1963@users.noreply.github.com>
2020-08-15 11:23:45 +0100

    Update Date Splitter.py and add Planning.md

--- Date Splitter.py
+++ Date Splitter.py
@@ -7,7 +7,27 @@ import shutil
 # ===== Initial Setup
 
 print()
+print("Welcome to the WhatsApp Date Splitter!")
+print()
 print("Please move the selected zip file to /venv/")
-print("This is yet to be coded.")
+print()
+input_file = input("Please enter the name of the input zip file (including .zip extension): ")
+print()
+recipName = input("Please enter the name of the recipient (case sensitive): ")
+print()
+outputDir = input("Please enter a full output directory: ")
+print()
+
+# Extracts selected zip file to /full_temp/
+zip_ref = zf.ZipFile(input_file)
+print("Unzipping...")
+zip_ref.extractall("full_temp")
+zip_ref.close()
+print("Unzipped!")
+print()
+
+#
+print()
+print("Complete!")
 
 #
---
+++ Planning.md
@@ -0,0 +1,20 @@
+# Planning
+
+Intended output is `{outputDir}/{recipName}/{recipName} - {month}.zip`
+For all months the original chat covers
+
+## Modules
+
+- Initial inputs
+- Unzip and rezip
+- Text date parser
+- File date parser
+- Cut section of _chat.txt to new _chat.txt in output directory
+- Cut files to output directory
+- Clear up
+
+## Steps
+
+1. Take initial inputs
+2. Unzip into `full_temp` folder
+3. Parse _chat.txt into months
