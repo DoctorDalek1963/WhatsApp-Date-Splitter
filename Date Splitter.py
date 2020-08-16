1d4326a8d8fd9a5eb50d71722620bb0156617af6
Doctor Dalek <69600500+DoctorDalek1963@users.noreply.github.com>
2020-08-16 15:13:26 +0100

    Update Date Splitter.py

--- Date Splitter.py
+++ Date Splitter.py
@@ -1,6 +1,6 @@
-import zipfile as zf
+from zipfile import ZipFile
 import os
-import glob as g
+from glob import glob
 import datetime as dt
 import shutil
 
@@ -14,16 +14,9 @@ print()
 input_file = input("Please enter the name of the input zip file (including .zip extension): ")
 print()
 recipName = input("Please enter the name of the recipient (case sensitive): ")
-print()
-outputDir = input("Please enter a full output directory: ")
-print()
-
-# Extracts selected zip file to /full_temp/
-zip_ref = zf.ZipFile(input_file)
-print("Unzipping...")
-zip_ref.extractall("full_temp")
-zip_ref.close()
-print("Unzipped!")
+# print()
+# outputDir = input("Please enter a full output directory: ")
+outputDir = os.getcwd() + "Output"
 print()
 
 # Make dir if it doesn't exist
@@ -33,6 +26,25 @@ except OSError:
     pass
 
 outputDir = f"{outputDir}/{recipName}"
+
+# Extracts selected zip file to /full_temp/
+zip_ref = ZipFile(input_file)
+print("Unzipping...")
+zip_ref.extractall(f"{outputDir}/full_temp")
+zip_ref.close()
+print("Unzipped!")
+print()
+
+with open(f"{outputDir}/full_temp/_chat.txt", encoding="utf-8") as f:
+    chat_txt_list = f.read().splitlines()
+
+
+# ===== Define functions
+
+
+def date_parser(string):
+    pass
+
 
 #
 print()
