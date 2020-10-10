136ba239c9f98920e7988b7da679f0b09dd6b587
Doctor Dalek <69600500+DoctorDalek1963@users.noreply.github.com>
2020-10-10 15:51:20 +0100

    Added GPL V3 License

--- Date-Splitter.py
+++ Date-Splitter.py
@@ -1,3 +1,22 @@
+# WhatsApp-Date-Splitter is a program that takes large exported WhatsApp
+# chats and splits them into months to make the zip files more manageable
+# for WhatsApp-Formatter <https://github.com/DoctorDalek1963/WhatsApp-Formatter>.
+#
+# Copyright (C) 2020 Doctor Dalek <https://github.com/DoctorDalek1963>.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from shutil import make_archive
 from datetime import datetime
 from zipfile import ZipFile
