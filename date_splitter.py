8445508ae5b6a717576a06619c51b23e11b1974b
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2020-12-27 10:25:14 +0000

    Rearrange imports

--- date_splitter.py
+++ date_splitter.py
@@ -17,8 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import os
 from splitter_functions import extract_zip, date_split, zip_up_split_folders
+import os
 
 # ===== Initial Setup
 
