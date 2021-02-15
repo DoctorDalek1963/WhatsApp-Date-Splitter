c85b182e604f78285c177eedc0be770077963904
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2021-02-15 23:41:26 +0000

    Wrap compile functionality in function

--- compile_gui.py
+++ compile_gui.py
@@ -5,23 +5,29 @@
 import shutil
 import os
 
-# Create temporary directory to hold everything
-os.mkdir('compile_temp')
 
-# Copy dependencies to temporary directory
-shutil.copy('icon.ico', 'compile_temp')
+def compile_date_splitter():
+    # Create temporary directory to hold everything
+    os.mkdir('compile_temp')
 
-# Run pyinstaller with correct flags from command prompt (I'm on Windows and haven't tested this on Linux or MacOS)
-os.system('cmd /c "pyinstaller date_splitter_gui.py -wF -n WhatsApp_Date_Splitter --distpath ./compile_temp -i '
-          'icon.ico"')
+    # Copy dependencies to temporary directory
+    shutil.copy('icon.ico', 'compile_temp')
 
-# Remove spec file (I don't think there's a flag to tell pyinstaller to not generate this)
-os.remove('WhatsApp_Date_Splitter.spec')
+    # Run pyinstaller with correct flags from command prompt (I'm on Windows and haven't tested this on Linux or MacOS)
+    os.system('cmd /c "pyinstaller date_splitter_gui.py -wF -n WhatsApp_Date_Splitter --distpath ./compile_temp -i '
+              'icon.ico"')
 
-# Zip up compiled program with dependencies
-shutil.make_archive('WhatsApp_Date_Splitter', 'zip', 'compile_temp')
+    # Remove spec file (I don't think there's a flag to tell pyinstaller to not generate this)
+    os.remove('WhatsApp_Date_Splitter.spec')
 
-# Clear and remove unnecessary directories
-shutil.rmtree('build')
-shutil.rmtree('compile_temp')
-shutil.rmtree('__pycache__')
+    # Zip up compiled program with dependencies
+    shutil.make_archive('WhatsApp_Date_Splitter', 'zip', 'compile_temp')
+
+    # Clear and remove unnecessary directories
+    shutil.rmtree('build')
+    shutil.rmtree('compile_temp')
+    shutil.rmtree('__pycache__')
+
+
+if __name__ == "__main__":
+    compile_date_splitter()
