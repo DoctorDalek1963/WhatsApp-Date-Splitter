c5d8d443886fedd13fef11fa6c058c524223a9f4
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2021-02-16 12:54:09 +0000

    Rename compile.py and make it better

--- compile.py
+++ compile.py
@@ -4,18 +4,24 @@
 
 import shutil
 import os
+import subprocess
 
 
-def compile_date_splitter():
+def compile_date_splitter(gui=True):
+    """Compile the WhatsApp Date Splitter using pyinstaller.
+
+This function takes one argument, gui, which is true by default. If it's true, this function will compile the GUI version of the formatter, if false, it will compile the command line version."""
+    # Get filename from gui boolean
+    filename = 'gui.py' if gui else 'cli.py'
+
     # Create temporary directory to hold everything
     os.mkdir('compile_temp')
 
     # Copy dependencies to temporary directory
     shutil.copy('icon.ico', 'compile_temp')
 
-    # Run pyinstaller with correct flags from command prompt (I'm on Windows and haven't tested this on Linux or MacOS)
-    os.system('cmd /c "pyinstaller date_splitter_gui.py -wF -n WhatsApp_Date_Splitter --distpath ./compile_temp -i '
-              'icon.ico"')
+    # Run pyinstaller with correct flags from the shell
+    subprocess.call(f'pyinstaller {filename} -wF -n WhatsApp_Date_Splitter --distpath ./compile_temp -i icon.ico', shell=True)
 
     # Remove spec file (I don't think there's a flag to tell pyinstaller to not generate this)
     os.remove('WhatsApp_Date_Splitter.spec')
@@ -29,5 +35,5 @@ def compile_date_splitter():
     shutil.rmtree('__pycache__')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     compile_date_splitter()
